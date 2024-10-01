1089e690c0d8abd4fcdd128478686b949afe7e9b
Jeferson Brandão <jefersonbrandaodenovaes@gmail.com>
2024-09-30 21:27:31 -0300

    Movendo indice

--- Hospital/Fila.cs
+++ Hospital/Fila.cs
@@ -56,6 +56,67 @@ namespace Hospital
                 }
             }
         }
+        public void ProcessarPaciente()
+        {
+            Console.WriteLine("\n--- Processar Paciente ---");
+
+            Console.WriteLine("Nome do paciente:");
+            string nomePaciente = Console.ReadLine();
+
+            Console.WriteLine("Tratamento:");
+            string tratamento = Console.ReadLine();
+
+            Console.WriteLine("Senha do paciente:");
+            int senhaPaciente = int.Parse(Console.ReadLine());
+
+
+            if (!string.IsNullOrEmpty(nomePaciente) && !string.IsNullOrEmpty(tratamento))
+            {
+                for (int i = 0; i < fila.Length; i++)
+                {
+                    if (!string.IsNullOrEmpty(fila[i]) && senhaPacientes[i] == senhaPaciente)
+                    {
+
+                        filaAdjacente[posFilaAdjacente] = $"Paciente: {nomePaciente}, Tratamento: {tratamento}, Senha: {senhaPaciente}";
+                        posFilaAdjacente++;
+
+                        Console.WriteLine("Paciente {0} foi processado e movido para a fila adjacente.", nomePaciente);
+
+
+                        fila[i] = null;
+                        senhaPacientes[i] = -1;
+
+
+                        for (int j = i; j < fila.Length - 1; j++)
+                        {
+                            fila[j] = fila[j + 1];
+                            senhaPacientes[j] = senhaPacientes[j + 1];
+                        }
+                        fila[fila.Length - 1] = null;
+                        senhaPacientes[fila.Length - 1] = -1;
+
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nome ou tratamento não podem ser nulos.");
+            }
+        }
+
+
+        public void ExibirFila()
+        {
+            Console.WriteLine("\n--- Fila Original ---");
+            for (int i = 0; i < fila.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(fila[i]))
+                {
+                    Console.WriteLine($"Paciente {i + 1}: {fila[i]} (Senha: {senhaPacientes[i]})");
+                }
+            }
+        }
 
     }
 }
